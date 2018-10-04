d814cfc8b14c165229c19865845b03b1530c1558
heenashrimali <heenashrimali973@gmail.com>
2018-10-04 23:15:13 +0530

    custom validation resolved

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,8 +5,8 @@ class Employee < ApplicationRecord
   validates :contact_no,uniqueness: true,length:{is: 10}
   validates :salary ,numericality: true
   validates :designation ,inclusion: { in: %w(Manager Clerk Casher DM PO)}
-  validates :check_age
-  validates :check_salary
+  validate :check_age
+  validate :check_salary
 
   private
   
